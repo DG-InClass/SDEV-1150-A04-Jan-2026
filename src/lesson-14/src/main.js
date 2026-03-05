2ce348ffab476ca82e0d9e335eb71a7e0fef7100
Dan Gilleland <dgilleland@nait.ca>
2026-03-05 13:53:48 -0700

    Complete lesson 14

--- src/lesson-14/src/main.js
+++ src/lesson-14/src/main.js
@@ -1,1 +1,15 @@
+import { greetUser } from './utils';
+import utilities from './utils';
+import dayjs from 'dayjs'; // Read the docs on dayjs
+//     \___/      \_____/
+//      |           | Name of the package/location for the JavaScript module
+//      | The function/object we are grabbing from this package.
+//        This is the "thing" that the 'dayjs' package exported as it's "default".
+// You can search for 3rd-party packages like dayjs on
+// npmjs.com and see the ReadMe for whatever packages
+// you have searched for.
 console.log('Lesson 14 starter loaded');
+const user = prompt('Enter your name') || utilities.defaultName;
+const greeting = greetUser(user);
+const currentDate = dayjs().format('dddd, MMMM, D, YYYY');
+document.querySelector('#today').textContent = `${greeting} Today is ${currentDate}`;
---
+++ src/lesson-14/src/utils.js
@@ -0,0 +1,5 @@
+export function greetUser(name) {
+    return `Welcome to the app, ${name}!`;
+}
+
+export default { defaultName: 'Jane Doe' };
