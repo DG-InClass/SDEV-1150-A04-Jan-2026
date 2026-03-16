76da622f567cb0a7e511cd55fffafa16c328b1e7
Dan Gilleland <dgilleland@nait.ca>
2026-03-16 14:10:03 -0600

    Fetching books in my web app

--- src/lesson-16/src/main.js
+++ src/lesson-16/src/main.js
@@ -1,13 +1,36 @@
 // Import the functions necessary to make the API calls
+import { fetchJsonData } from './utils';
 
 // Select the necessary DOM elements
+const loadButton = document.getElementById('loadBooks');
+const list = document.getElementById('bookList');
 
 // Define the API endpoint
+const endpoint = 'http://localhost:3000/books'; // Note: This might be something you put outside of the main.js, depending on how you design your app.
 
 // Define a function to handle loading and displaying the list of books
+const loadHandler = function() {
+    // Note: A Promise<T> has two associated functions:
+    //   .then(), which also returns a Promise<T> - when the promise succeeds
+    //   .catch(), which returns a Promise<T> - when the promise fails (is rejected)
+    fetchJsonData(endpoint)
+        .then(data => {
+            // 😁 Looks like we got data!
+            console.log(data);
+            list.innerHTML = ''; // clear the existing content
+            data.forEach(book => {
+                // I'll use the DOM API for creating elements
+                const li = document.createElement('li');
+                li.textContent = `${book.title} by ${book.author}`;
+                list.appendChild(li);
+            });
+        });
+}
+
 
 // Define a function to handle form submission for adding a new book
 
 // Attach event listeners to the button and form
+loadButton.addEventListener('click', loadHandler);
 
 // TODO: Add delete functionality
--- src/lesson-16/src/utils.js
+++ src/lesson-16/src/utils.js
@@ -1,4 +1,25 @@
 // Fetch utility function
+// ⚠️ NOTE: The version of `fetchData()` that I am showing you differs from the version shown in the `README.md` docs for this demo.
+export function fetchJsonData(endpoint) {
+    return fetch(endpoint)
+            .then(handleResponse);
+}
+
+// This is a private function that will do the "details" of handling a server response that is supposed to return JSON information.
+/**
+ * Checks that the response is ok (status code 200)
+ * and returns a JavaScript object from the JSON data.
+ * @param {Response} response An HTTP Response from some web server
+ * @returns {object}
+ */
+const handleResponse = function(response) {
+    if(!response.ok) {
+        // Uh-oh, something went wrong
+        let message = `Server responsed with ${response.status} (${response.statusText}). (${response.url})`;
+        throw new Error(message); // In JavaScript, throwing an exception will automatically exit the function.
+    }
+    return response.json(); // Parse the body of the response as though it is JSON and return the JavaScript object/array.
+}
 
 // POST utility function
 
