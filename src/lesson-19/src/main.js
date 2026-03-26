06d8068421294122a86a552254655cec785b205b
Dan Gilleland <dgilleland@nait.ca>
2026-03-26 13:40:32 -0600

    Clean up previous code for finishing lesson 19

--- src/lesson-19/src/main.js
+++ src/lesson-19/src/main.js
@@ -1,10 +1,10 @@
-// Ad-hoc code
-const template = document.getElementById('user-card-template');
+// // Ad-hoc code
+// const template = document.getElementById('user-card-template');
 
-const content = template.content.cloneNode(true); // "copy" of the contents
-// debugger;
-document.body.appendChild(content); // Voila! We've got the content dynamically added.
-// ... end of Ad-hoc
+// const content = template.content.cloneNode(true); // "copy" of the contents
+// // debugger;
+// document.body.appendChild(content); // Voila! We've got the content dynamically added.
+// // ... end of Ad-hoc
 
 // Import the user-card component to register the custom element
 import './user-card'; // Notice in our import, we aren't grabbing the exported type, but the code that declares our <user-card> will still run.
