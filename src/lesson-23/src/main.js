6706843358b89f8007e70a675c31ac898529d141
Dan Gilleland <dgilleland@nait.ca>
2026-04-02 14:40:59 -0600

    Completed lesson 23

--- src/lesson-23/src/main.js
+++ src/lesson-23/src/main.js
@@ -1,6 +1,26 @@
 // Import the user-card component to register the custom element
 import './user-card.js';
 
+let main = document.querySelector('main');
+let myCard = document.createElement('user-card');
+main.appendChild(myCard);
+// console.log(myCard.#followed); // Private members can't be accessed
+// console.log(myCard._onFollow);
+
+//An array of user objects
+const users = [
+  { id: 'u1', name: 'Zelda', avatar: 'assets/zelda-avatar.png', description: 'Princess of Hyrule' },
+  { id: 'u2', name: 'Link', avatar: 'assets/link-avatar.png', description: 'Hero of Hyrule'},
+  { id: 'u3', name: 'Mipha', description: 'Zora Champion' }
+];
+
+users.forEach(user => {
+  const card = document.createElement('user-card');
+  card.user = user;
+  main.appendChild(card);
+});
+
+
 // Theme toggle button logic
 let dark = false;
 const toggleBtn = document.querySelector('#btn-theme');
--- src/lesson-23/src/user-card.js
+++ src/lesson-23/src/user-card.js
@@ -54,47 +54,92 @@ template.innerHTML = `
 document.body.appendChild(template);
 
 class UserCard extends HTMLElement {
+  // Private Field/Property of the UserCard
+  #followed; // = false; // initial value
+  #btn; // If you create an uninitialized field/property,
+        // then you should make sure to give it a value
+        // inside the constructor() function.
+  #user = null; // This will be more "complete" info on the user
+
+  // With the #user being private, I can create a setter/getter to
+  // change/access the underlying data
+  get user() { return this.#user; }
+  set user(userInfo) {
+    // TODO: Perform some data validation on the userInfo
+    this.#user = userInfo; // if it passes validation, store it
+    // Render the UI (assume user details have changed)
+    this.#renderFromUser();
+  }
+
+  #renderFromUser() {
+    if(this.#user) {
+      if(this.#user.avatar) {
+        this.#img.src = this.#user.avatar;
+      } else {
+        this.#img.src = 'https://placehold.co/80x80/0077ff/ffffff';
+      }
+      // NOTE: Compare the approach above with the one below.
+      //       Which one makes better sense to you? Which do you prefer?
+      this.setAttribute('user-id', this.#user.id || '');
+
+      // Update internal slots via shado DOM query selectors for the text nodes.
+      const nameSlot = this.shadowRoot.querySelector('[name="name"]');
+      if(nameSlot) {
+        nameSlot.textContent = this.#user.name || '';
+      }
+      const descSlot = this.shadowRoot.querySelector('[name="description"]');
+      if(descSlot) {
+        descSlot.textContent = this.#user.description || '';
+      }
+    }
+  }
+
+  #img;
+
   constructor() {
     super();
 
     // Added property to track follow state
-    this._followed = false;
+    this.#followed = false;
 
     const shadow = this.attachShadow({ mode: 'open' });
     const content = template.content.cloneNode(true);
-    const img = content.querySelector('img');
-    img.src = this.getAttribute('avatar') || 'https://placehold.co/80x80/0077ff/ffffff';
-    this._btn = content.querySelector('button');
-    this._btn.addEventListener('click', () => this._onFollow());
+    this.#img = content.querySelector('img');
+    // #img.src = this.getAttribute('avatar') || 'https://placehold.co/80x80/0077ff/ffffff';
+    this.#btn = content.querySelector('button');
+    this.#btn.addEventListener('click', () => this.#onFollow());
     shadow.appendChild(content);
   }
 
   follow() {
-    this._setFollow(true);
+    this.#setFollow(true);
   }
 
   unfollow() {
-    this._setFollow(false);
+    this.#setFollow(false);
   }
 
   // Property to read followed state
+  // On the inside of the UserCard, this looks (and acts) as a function
+  // BUT, because it has the get keyword, it will be used from the "outside"
+  // as though it is just a property.
   get followed() {
-    return this._followed;
+    return this.#followed;
   }
 
-  _setFollow(value) {
-    this._followed = value;
-    this._btn.textContent = this._followed ? 'Following' : 'Follow';
+  #setFollow(value) {
+    this.#followed = value;
+    this.#btn.textContent = this.#followed ? 'Following' : 'Follow';
     this.dispatchEvent(new CustomEvent('follow-change', {
-      detail: { id: this.getAttribute('user-id') || null, followed: this._followed },
+      detail: { id: this.getAttribute('user-id') || null, followed: this.#followed },
       bubbles: true,
       composed: true,
     }));
   }
 
   // Follow button handler
-  _onFollow() {
-    this._setFollow(!this._followed);
+  #onFollow() {
+    this.#setFollow(!this.#followed);
   }
 
   // Respond to attribute changes if needed in the future
@@ -109,6 +154,30 @@ class UserCard extends HTMLElement {
         img.src = newValue;
       }
     }
+  }
+
+  // Lifecyle: called by the browser when the element is added to the DOM
+  connectedCallback() {
+    // Attach local listener(s)
+    this.#btn.addEventListener('click', this.#onButtonClick);
+
+    // If user property was set before connect, render it now
+    if (this.#user) {
+      this.#renderFromUser();
+    } else {
+      // Fallback to attribute if user property not proviced
+      const avatar = this.getAttribute('avatar');
+      this.#img.src = avatar || 'https://placehold.co/80x80/0077ff/ffffff';
+    }
+  }
+
+  // Lifecycle: called by the browser when the element is removed from the DOM
+  disconnectedCallback() {
+    this.#btn.removeEventListener('click', this.#onButtonClick);
+  }
+
+  #onButtonClick() {
+    this.#setFollow(!this.#followed); // Toggle the follow state
   }
 }
 
