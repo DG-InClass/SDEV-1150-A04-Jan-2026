4fb31023c5b3a73e5a7a6b43a27fec64c8a8ad4e
Dan Gilleland <dgilleland@nait.ca>
2026-04-09 14:36:45 -0600

    More unit tests

--- src/lesson-24/tests/user-card.spec.js
+++ src/lesson-24/tests/user-card.spec.js
@@ -8,14 +8,59 @@ import '../src/user-card'; // import the web component definition
 describe('UserCard component', () => {
     /* My typical workflow when designing/developing some component is to
        list out all my test/spec names
-       - should not be following the character by default
-       - render name correctly
-       - render description correctly
+       - 
+       - 
+       - 
 
     */
-    it.todo('', () => {});
-    it.todo('', () => {});
-    it.todo('', () => {});
+    it('should not be following the character by default', () => {
+        // Arrange
+        const element = document.createElement('user-card');
+        document.body.appendChild(element);
+
+        // Act
+        let actual = element.followed;
+
+        // Assert
+        expect(actual).toBe(false);
+    });
+
+    it('render name correctly', () => {
+        // Arrange
+        // 1) Create the custom element
+        const element = document.createElement('user-card');
+        // 2) <span slot="name">Vitest user</span>
+        const nameSpan = document.createElement('span');
+        nameSpan.setAttribute('slot', 'name');
+        nameSpan.textContent = 'Vitest user';
+        // 3) Put the slotted content into the custom element 😢
+        element.appendChild(nameSpan);
+        // 4) Put the custom element into the (fake) DOM
+        document.body.appendChild(element);
+
+        // Act
+        const actual = element.shadowRoot.querySelector('slot[name="name"]');
+
+        // Assert
+        expect(actual.assignedNodes()[0].textContent).toBe('Vitest user');
+    });
+
+    it('render description correctly', () => {
+        // Arrange
+        // 1) Create the custom element
+        const element = document.createElement('user-card');
+        const nameSpan = document.createElement('span');
+        nameSpan.setAttribute('slot', 'description');
+        nameSpan.textContent = 'Someone who is comfortable writing unit tests';
+        element.appendChild(nameSpan);
+        document.body.appendChild(element);
+
+        // Act
+        const actual = element.shadowRoot.querySelector('slot[name="description"]');
+
+        // Assert
+        expect(actual.assignedNodes()[0].textContent).toBe('Someone who is comfortable writing unit tests');
+    });
 
     it('should render with default properties for the image', () => {
         // Arrange - Setup the stuff around what we testing
