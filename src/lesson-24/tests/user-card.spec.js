62e7c150d04960337e5b3a02df5409d6cea429e5
Dan Gilleland <dgilleland@nait.ca>
2026-04-09 14:50:25 -0600

    beforeEach and afterEach + practice work

--- src/lesson-24/tests/user-card.spec.js
+++ src/lesson-24/tests/user-card.spec.js
@@ -2,21 +2,38 @@
 // ~/tests/user-card.spec.js
 // In the terminal, run:  pnpm test
 //                   or:  pnpm test:watch
-import { expect, it, test, describe } from 'vitest';
+import { expect, it, test, beforeEach, afterEach, describe } from 'vitest';
 import '../src/user-card'; // import the web component definition
 
+// The describe is used to group related tests in a "test suite"
 describe('UserCard component', () => {
+    let element; // A variable shared by all the tests in this test suite
+
+    beforeEach(() => {
+        // Common setup for a "clean" start on each test
+        document.body.innerHTML = ''; // One way to "clean" fake DOM
+        element = document.createElement('user-card');
+        document.body.appendChild(element);
+    });
+
+    afterEach(() => {
+        // Cleanup after each test
+        element.remove();
+        element = null;
+    });
+
     /* My typical workflow when designing/developing some component is to
        list out all my test/spec names
-       - 
-       - 
-       - 
+       - should set the image.src based on the avatar attribute
+          <user-card avatar="https://marvel.comics.com/wolverine.png">
+       - should change followed status to true programmatically
+            element.follow()
+       - should change followed status to false programmatically
+            element.unfollow()
 
     */
     it('should not be following the character by default', () => {
         // Arrange
-        const element = document.createElement('user-card');
-        document.body.appendChild(element);
 
         // Act
         let actual = element.followed;
@@ -27,16 +44,12 @@ describe('UserCard component', () => {
 
     it('render name correctly', () => {
         // Arrange
-        // 1) Create the custom element
-        const element = document.createElement('user-card');
         // 2) <span slot="name">Vitest user</span>
         const nameSpan = document.createElement('span');
         nameSpan.setAttribute('slot', 'name');
         nameSpan.textContent = 'Vitest user';
         // 3) Put the slotted content into the custom element 😢
         element.appendChild(nameSpan);
-        // 4) Put the custom element into the (fake) DOM
-        document.body.appendChild(element);
 
         // Act
         const actual = element.shadowRoot.querySelector('slot[name="name"]');
@@ -47,13 +60,10 @@ describe('UserCard component', () => {
 
     it('render description correctly', () => {
         // Arrange
-        // 1) Create the custom element
-        const element = document.createElement('user-card');
         const nameSpan = document.createElement('span');
         nameSpan.setAttribute('slot', 'description');
         nameSpan.textContent = 'Someone who is comfortable writing unit tests';
         element.appendChild(nameSpan);
-        document.body.appendChild(element);
 
         // Act
         const actual = element.shadowRoot.querySelector('slot[name="description"]');
@@ -64,9 +74,6 @@ describe('UserCard component', () => {
 
     it('should render with default properties for the image', () => {
         // Arrange - Setup the stuff around what we testing
-        const element = document.createElement('user-card');
-        document.body.appendChild(element);
-
         // Act - Do the "thing" that we are testing
         const actual = element.shadowRoot.querySelector('img');
 
